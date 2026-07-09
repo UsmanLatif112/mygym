c669ff86a8b0c2304140c9c366312be8f78ee0f1
Usman0026 <usman@aitomation.com>
2026-07-09 17:36:17 +0500

    fix: update PyInstaller command for desktop launcher to use onefile option

--- desktop_launcher.py
+++ desktop_launcher.py
@@ -40,5 +40,4 @@ if __name__ == "__main__":
 
 
 
-'''pyinstaller --onedir --noconsole --add-data "templates;templates" --add-data "static;static" --add-data ".env;." desktop_launcher.py
-'''
+'''pyinstaller --onefile --noconsole --icon "E:\Alpha fitness gym\mygym\static\logo.ico" --add-data "templates;templates" --add-data "static;static" --add-data ".env;." desktop_launcher.py'''
